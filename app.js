4aa8cb6a8b8dfd9460ad01751fe16901fb325e02
labMySt <33463536+labMySt@users.noreply.github.com>
2018-05-13 11:45:31 +0300

    session and connection with mongoDB

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const passport = require("passport");
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const config = require('config');
+//======================================================================
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const sessoinSecret = config.get('Customer.session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//======================================================================
+require("./database");
 
 var app = express();
-
+var db = mongoose.connection;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -17,8 +27,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const myStore = new MongoStore({
+    mongooseConnection: db
+  });
+app.use(session({
+secret: sessoinSecret,
+store:  myStore,
+resave: false,
+proxy:  true,
+saveUninitialized: false
+}));
+//==================================== routes ============================
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ database/index.js
@@ -0,0 +1,68 @@
+var mongoose = require('mongoose');
+var config = require('config');
+var dbConfig = config.get('Customer.dbConfig');
+
+
+var mongoose = require("mongoose");
+
+mongoose.connect(dbConfig);
+
+mongoose.connection.on('connected', function() {
+	console.log('Mongoose connected to ' + dbConfig);
+});
+
+mongoose.connection.on('error', function(err) {
+	console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+	console.log('Mongoose disconnected');
+});
+
+process.once('SIGUSR2', function() {
+	gracefulShutdown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+process.on('SIGINT', function() {
+	gracefulShutdown('app termination', function() {
+		process.exit(0);
+	});
+});
+
+process.on('SIGTERM', function() {
+	gracefulShutdown('app shutdown', function() {
+		process.exit(0);
+	});
+});
+
+var gracefulShutdown = function(msg, callback) {
+	mongoose.connection.close(function() {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+// var Schema = mongoose.Schema;
+//
+// var userSchema = new Schema({
+//   name: String,
+//   email: String,
+//   password: String
+// })
+//
+// mongoose.connect(URIooll);
+//
+// var User = mongoose.model("User", userSchema);
+// var user = new User({
+//   name: "John",
+//   email: "a@gmail.com",
+//   password: "1234"
+// });
+//
+// User.find({}, function(err, doc){
+//     mongoose.disconnect();
+//     if(err) return console.log(err);
+//     console.log(doc);
+// });
+// require('./user');
